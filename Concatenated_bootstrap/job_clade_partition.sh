d974fec2463d9127e524b6ae1bc5785a0b379d2a
Ananth Miller-Murthy <Ananth@Ananths-MacBook-Air.local>
2020-12-09 11:08:05 -0500

    Created partition file 'NADH+Rag-1.partitions' and new job script 'job_clade_partition.sh' to run IQTREE using the model selected from the last run of the concatenated tree (TIM2+F+R4), but with the partition between the two genes specified

---
+++ Concatenated_bootstrap/job_clade_partition.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --partition=eeb354
+#SBATCH --job-name=alignment_fasta
+#SBATCH --time=2:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+
+module load IQ-TREE/1.6.12
+
+iqtree -s NADH+Rag-1.aligned.fasta -p NADH+Rag-1.partitions -m TIM2+F+R4 -bb 1000 -nt AUTO
